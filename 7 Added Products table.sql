8e6714698bf6146f80c09bd9a18cbbdf711ba9fb
Vitalii <besteffects@gmail.com>
2019-04-08 19:23:46 -0300

    Added some contant to table Orders

--- 7 Added Products table.sql
+++ 7 Added Products table.sql
@@ -9,15 +9,23 @@ add Price float;
 
 
 insert into Products (ProductName, Price) values ('Baseball', 5.95);
-insert into Products (ProductName, Price) values ('Baseball', 195.99)
+insert into Products (ProductName, Price) values ('Baseball', 195.99);
 
+--Created table Orders
 create table Orders
 (
 OrderId int primary key identity (1,1),
 OrderDate Datetime,
-CustomerID INT,
+CustomerID int,
 ProductID int,
-)
+);
+
+select * from Orders;
+select * from Products;
+select * from Customer;
+
+insert into Orders (OrderDate, CustomerID, ProductID)
+values (GETDATE(),3,1)
 
 select * from Products;
 select * from Orders;
