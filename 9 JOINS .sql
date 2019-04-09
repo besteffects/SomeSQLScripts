dd330c4ccbea9a39d1322760aaf6d596e63f3359
Vitalii <besteffects@gmail.com>
2019-04-08 22:10:54 -0300

    Inner join from 3 tables

--- 9 JOINS .sql
+++ 9 JOINS .sql
@@ -3,5 +3,35 @@ select * from Products;
 select * from Customer;
 
 select * from Orders as o
-inner join Products as p on
-o.ProductID=p.id;
+inner join Products as p on o.ProductID=p.id;
+
+select o.*, p.*, c.*
+from Orders o
+inner join Products as p on o.ProductID=p.id
+inner join Customer c on o.CustomerID=C.Id;
+
+select o.OrderDate, p.ProductName, c.*
+from Orders o
+inner join Products as p on o.ProductID=p.id
+inner join Customer c on o.CustomerID=C.Id;
+
+
+--calculate the sum of all orders. As total is optional. Without it column will not have a name
+select sum(p.Price) as Total
+from Orders o
+inner join Products as p on o.ProductID=p.id
+inner join Customer c on o.CustomerID=C.Id;
+
+-- use GROUP BY with functions!
+select c.LastName, sum(p.Price) as Total
+from Orders o
+inner join Products as p on o.ProductID=p.id
+inner join Customer c on o.CustomerID=C.Id
+group by c.LastName;
+
+--inner join from 3 tables
+select c.LastName, p.ProductName, sum(p.Price) as Total
+from Orders o
+inner join Products as p on o.ProductID=p.id
+inner join Customer c on o.CustomerID=C.Id
+group by c.LastName, p.ProductName;
